7efed908542fe818d6e7545f3508df8c3e07a69b
Tomas Dohnalek <dohnto@gmail.com>
2013-09-25 00:33:13 +0200

    Imported relevant packages.

--- src/gps/GeneralPuzzleSolver.java
+++ src/gps/GeneralPuzzleSolver.java
@@ -1,10 +1,12 @@
 package gps;
 
+import gps.solver.*;
+import gps.statemanager.*;
+
 public class GeneralPuzzleSolver {
 
 	public static void main(String[] args) {
 		// TODO Automaticky generovaný stub metody
-		System.out.println("hello ");
+		System.out.println("hello");
 	}
-
 }
