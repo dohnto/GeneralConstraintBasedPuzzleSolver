0f372858ab914da6d4077db532650c21f07e0fb2
Tomas Dohnalek <dohnto@gmail.com>
2013-09-24 23:35:49 +0200

    Created abstract methods for LocalStateManager.

--- src/gps/statemanager/LocalStateManager.java
+++ src/gps/statemanager/LocalStateManager.java
@@ -2,4 +2,11 @@ package gps.statemanager;
 
 public abstract class LocalStateManager {
 
+	// TODO return type not void
+	//public abstract void getInitialState();
+	//public abstract void getRandomNeighbour(state);
+	//public abstract void getRandomNeighbour(state, count);
+	//public abstract void getInteligentNeighbour(state);
+	//public abstract int  evaluateState(state);
+	//public abstract void displayState(state);
 }
