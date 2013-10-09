3aa15b4eafa9259cfc9c5af1cc63b53c10184292
Jan Bednarik <ja.bedna1@gmail.com>
2013-10-09 03:36:38 +0200

    min-conflicts a K-queens done

--- src/gps/solver/MinConflicts.java
+++ src/gps/solver/MinConflicts.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public abstract class MinConflicts extends ConstraintBasedLocalSearch {        
     
     /* PROPERTIES */   
-    private int MAX_STEPS = 1;
+    private int MAX_STEPS = 10000;
     
     /* METHODS */   
     
@@ -31,10 +31,15 @@ public abstract class MinConflicts extends ConstraintBasedLocalSearch {
         // Initial puzzle state - randomly initializes the variables
         state = stateManager.getInitialState();
         
+//        System.err.println("INITIAL STATE:");
+//        stateManager.printState(state);
+        
         // Main loop
         while (step++ < MAX_STEPS) {                        
             // Get conflicting variables
             ArrayList<Integer> conflicts = stateManager.getConflicts(state);
+            
+            //System.out.println("Conflicts: " + conflicts);
             
             // Check whether puzzle is solved
             if(conflicts.isEmpty()) break;
@@ -43,11 +48,14 @@ public abstract class MinConflicts extends ConstraintBasedLocalSearch {
             int idx  = LocalStateManager.getRandomInt(0, conflicts.size() - 1);
             int c    = conflicts.get(idx);
             
+            //System.out.println("Choosing queen " + idx);
+            
             // Set new (better) value for the conflicting variable
             state = stateManager.getInteligentNeighbour(state, c);            
         }
         
-        stateManager.printState(state);
+//        System.err.println("FINAL STATE:");
+//        stateManager.printState(state);
         
         return state;
     }            
--- src/gps/statemanager/kqueens/KQueensStateManager.java
+++ src/gps/statemanager/kqueens/KQueensStateManager.java
@@ -3,6 +3,8 @@ package gps.statemanager.kqueens;
 import gps.statemanager.LocalStateManager;
 import java.util.ArrayList;
 import gps.Range;
+import static gps.statemanager.LocalStateManager.getRandomInt;
+import java.util.Collections;
 import java.util.HashSet;
 
 public class KQueensStateManager extends LocalStateManager {
@@ -32,6 +34,12 @@ public class KQueensStateManager extends LocalStateManager {
         return state;
     }
     
+    private boolean isConflict(int qi, int qj, ArrayList<Integer> state) {
+        return (state.get(qi) ==  state.get(qj))                      || 
+               (state.get(qi) == (state.get(qj) + Math.abs(qi - qj))) ||
+               (state.get(qi) == (state.get(qj) - Math.abs(qi - qj)));
+    }
+    
     @Override
     public ArrayList<Integer> getConflicts(ArrayList<Integer> state) {
         HashSet conflicts = new HashSet();
@@ -39,10 +47,7 @@ public class KQueensStateManager extends LocalStateManager {
         for(int qi = 0; qi < size; qi++) {            
             for(int qj = qi + 1; qj < size; qj++) {                
                 // check conflicts - 2 queens in the same column or diagonal
-                if((state.get(qi) ==  state.get(qj)) || 
-                   (state.get(qi) == (state.get(qj) + Math.abs(qi - qj))) ||
-                   (state.get(qi) == (state.get(qj) - Math.abs(qi - qj)))) {
-                    
+                if(isConflict(qi, qj, state)) {                    
                     conflicts.add(qi);
                     conflicts.add(qj);
                 }
@@ -54,10 +59,41 @@ public class KQueensStateManager extends LocalStateManager {
         
     @Override
     public ArrayList<Integer> getInteligentNeighbour(ArrayList<Integer> state, 
-                                                     int variable) {
-        ArrayList<Integer> newState = new ArrayList(state);
+                                                     int conflictQueen) {
+        ArrayList<Integer> newState  = new ArrayList(state);
+        ArrayList<Integer> conflicts = new ArrayList<>();
+        for(int i = 0; i < size; i++) conflicts.add(0);
         
+        // get conflicts for all possible queen's positions
+        for(int i = 0; i < size; i++) {
+            newState.set(conflictQueen, i);
+            for(int otherQueen = 0; otherQueen < size; otherQueen++) {                
+                if(isConflict(conflictQueen, otherQueen, newState))
+                    conflicts.set(i, conflicts.get(i) + 1);
+            }
+        }
         
+        // place queen to the best position
+        int currentConflicts = conflicts.get(state.get(conflictQueen));        
+        int minConflicts     = Collections.min(conflicts);
+        
+        if(currentConflicts == minConflicts) { /* no better position */
+            ArrayList<Integer> possibleColumns = new ArrayList<>();
+            for(int c = 0; c < size; c++) {
+                if(conflicts.get(c) == minConflicts) {
+                    possibleColumns.add(c);           
+                }
+            }
+            
+            // randomly choose one of the other best positions (if any)
+            possibleColumns.remove(state.get(conflictQueen));
+            if(!possibleColumns.isEmpty()) {
+                int column = getRandomInt(0, possibleColumns.size() - 1);
+                state.set(conflictQueen, possibleColumns.get(column));
+            }
+        } else {    /* better position found */
+            state.set(conflictQueen, conflicts.indexOf(minConflicts));
+        }
         
         return state;
     }
